085d4076de0fcb658e4fc8c6f5b98e38d92ad392
Michael Woo <302maik@gmail.com>
2023-02-13 21:26:50 +0100

    Added movement boundaries to PlayerMovement behaviour

--- Assets/_Scripts/Behaviours/Player/PlayerBehaviour.cs
+++ Assets/_Scripts/Behaviours/Player/PlayerBehaviour.cs
@@ -6,12 +6,19 @@ using UnityEngine.InputSystem;
 public class PlayerBehaviour : MonoBehaviour {
 
     [SerializeField]
-    private float _speed = 5f;
+    float _speed = 5f;
+    [SerializeField]
+    Boundaries _boundaries;
+    [SerializeField]
+    SpriteRenderer _playerSpriteRenderer;
 
     Vector2 _movementDirection;
+    float _spriteMiddlePoint;
 
     void Awake() {
         _movementDirection = new Vector2();
+        // Get the middle point of the square-shapped sprite of this GameObject
+        _spriteMiddlePoint = _playerSpriteRenderer.bounds.size.x * .5f;
     }
 
     // Start is called before the first frame update
@@ -24,11 +31,17 @@ public class PlayerBehaviour : MonoBehaviour {
         Move(_movementDirection);
     }
 
-    void Move(Vector2 movementDirection) {
-        transform.Translate(movementDirection * _speed * Time.deltaTime);
+    void Move(Vector2 direction) {
+        var movementDirection = direction * _speed * Time.deltaTime;
+        var boundedMovementDirection = new Vector2(
+            Mathf.Clamp(transform.position.x + movementDirection.x, _boundaries.LeftBoundary.position.x + _spriteMiddlePoint, _boundaries.RightBoundary.position.x - _spriteMiddlePoint),
+            Mathf.Clamp(transform.position.y + movementDirection.y, _boundaries.DownBoundary.position.y + _spriteMiddlePoint, _boundaries.UpBoundary.position.y - _spriteMiddlePoint)
+        );
+
+        transform.position = boundedMovementDirection;
     }
 
     void OnMove(InputValue value) {
-        _movementDirection = value.Get<Vector2>();
+        _movementDirection = value.Get<Vector2>().normalized;
     }
 }
