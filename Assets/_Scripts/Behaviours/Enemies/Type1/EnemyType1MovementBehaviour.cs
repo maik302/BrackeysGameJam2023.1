77cabf167f8dab2e7a514dc01a97fb90278e5991
Michael Woo <302maik@gmail.com>
2023-02-14 13:15:09 +0100

    Created Enemy Type 1 and its basic movement behaviour component

---
+++ Assets/_Scripts/Behaviours/Enemies/Type1/EnemyType1MovementBehaviour.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyType1MovementBehaviour : MonoBehaviour {
+
+    [SerializeField]
+    float _speed = 5f;
+    [SerializeField]
+    Boundaries _boundaries;
+    [SerializeField]
+    SpriteRenderer _playerSpriteRenderer;
+
+    float _spriteMiddlePoint;
+
+    void Awake() {
+        // Get the middle point of the square-shapped sprite of this GameObject
+        _spriteMiddlePoint = _playerSpriteRenderer.bounds.size.x * .5f;
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Move(Vector2.down);
+    }
+
+    void Move(Vector2 direction) {
+        var movementDirection = direction * _speed * Time.deltaTime;
+        var boundedMovementDirection = new Vector2(
+            Mathf.Clamp(transform.position.x + movementDirection.x, _boundaries.LeftBoundary.position.x + _spriteMiddlePoint, _boundaries.RightBoundary.position.x - _spriteMiddlePoint),
+            Mathf.Clamp(transform.position.y + movementDirection.y, _boundaries.DownBoundary.position.y + _spriteMiddlePoint, _boundaries.UpBoundary.position.y - _spriteMiddlePoint)
+        );
+
+        transform.position = boundedMovementDirection;
+    }
+}
